1b15dd42cc1ad16a64b943a116366cd1037b0e67
Minifauna <wolf-thymine.0n@icloud.com>
2025-04-08 13:53:50 -0400

    Add files via upload

---
+++ Day 5/For Loops/task.py
@@ -0,0 +1,16 @@
+fruits = ["Apple", "Peach", "Pear"]
+# Pause 1 - predict what will be printed: fruits + pies
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+# Prints each fruit then fruit pie, for each fruit in list "fruits"
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print("Hello")
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+print("Hello")
