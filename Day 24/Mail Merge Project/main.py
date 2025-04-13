0dded8311499d88ada9bb0c93f9a28fc96c8bf7a
Minifauna <wolf-thymine.0n@icloud.com>
2025-04-12 21:59:18 -0400

    state guessing game finished

--- Day 24/Mail Merge Project/main.py
+++ Day 24/Mail Merge Project/main.py
@@ -6,7 +6,7 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
+PLACEHOLDER = '[name]'
 names = []
 with open('Input/Names/invited_names.txt') as file:
     txt = file.readlines()
@@ -14,14 +14,12 @@ with open('Input/Names/invited_names.txt') as file:
         names.append(name[:-1])
     names.append(txt[-1])
 
-outbound = []
 with open('Input/Letters/starting_letter.txt') as file:
     message = file.readlines()
     template = message[0]
     print(template)
     for name in names:
         with open(f'Output/ReadyToSend/{name} Invite.txt', 'w') as output:
-            output.write(template.replace('[name]', f'{name}'))
+            output.write(template.replace(PLACEHOLDER, f'{name}'))
         with open(f'Output/ReadyToSend/{name} Invite.txt', 'a') as outbound:
             outbound.write(''.join(message[1:]))
-
